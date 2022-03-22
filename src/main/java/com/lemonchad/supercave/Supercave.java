8fd049b65281bad7d258f53bd2b0a3b84aea735b
Jack <jakitmationstudios@gmail.com>
2022-03-22 14:31:19 -0400

    Lean enderman (I'm sorry)

--- src/main/java/com/lemonchad/supercave/Supercave.java
+++ src/main/java/com/lemonchad/supercave/Supercave.java
@@ -7,16 +7,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.boss.BossBar;
-import org.bukkit.entity.Arrow;
-import org.bukkit.entity.Skeleton;
-import org.bukkit.entity.Spider;
-import org.bukkit.entity.Zombie;
+import org.bukkit.entity.*;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionData;
+import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.potion.PotionType;
 import org.bukkit.util.Vector;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -87,6 +86,22 @@ public final class Supercave extends JavaPlugin {
                     });
                 }, 20 + 2 * r);
             }
+        }, 5);
+        LeanMobGenerator.register(Enderman.class, e -> {
+            e.getWorld().playSound(e.getLocation(), Sound.ENTITY_ENDERMAN_SCREAM, 1, 0.5f);
+            e.getWorld().spawnParticle(Particle.END_ROD, e.getLocation().clone().add(0, 1.5, 0), 15, 0.5, 1.5, 0.5, 0);
+
+            Bukkit.getScheduler().runTaskLater(INSTANCE, () -> {
+                if (e.isDead()) return;
+                Collection<Entity> entities = e.getNearbyEntities(8, 8, 8);
+                entities.forEach(entity -> {
+                    e.getWorld().playSound(entity.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1, 1);
+                    e.getWorld().spawnParticle(Particle.PORTAL, entity.getLocation(), 15, 0.5, 0.5, 0.5, 0);
+                    entity.teleport(e.getLocation());
+                    e.getWorld().spawnParticle(Particle.PORTAL, entity.getLocation(), 15, 0.5, 0.5, 0.5, 0);
+                });
+                e.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 20 * 5, 3));
+            }, 20);
         }, 5);
     }
 
