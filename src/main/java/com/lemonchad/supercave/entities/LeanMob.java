abfec153331bd20e6e028ead75538699cd8fe6bc
Jack <jakitmationstudios@gmail.com>
2022-03-22 14:30:06 -0400

    No burning

--- src/main/java/com/lemonchad/supercave/entities/LeanMob.java
+++ src/main/java/com/lemonchad/supercave/entities/LeanMob.java
@@ -2,11 +2,15 @@ package com.lemonchad.supercave.entities;
 
 import com.lemonchad.supercave.Supercave;
 import com.lemonchad.supercave.items.SupercaveItems;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.entity.Monster;
+import org.bukkit.entity.Skeleton;
+import org.bukkit.entity.Zombie;
 
 public class LeanMob<T extends Monster> {
     private final T entity;
@@ -29,6 +33,16 @@ public class LeanMob<T extends Monster> {
         decisionBoundary = Math.random();
         engaged = false;
 
+        if (entity instanceof Skeleton) {
+            ((Skeleton) entity).setShouldBurnInDay(false);
+        } else if (entity instanceof Zombie) {
+            ((Zombie) entity).setShouldBurnInDay(false);
+        }
+
+        thread = Bukkit.getScheduler().scheduleSyncRepeatingTask(Supercave.INSTANCE, this::tick, 0, 1);
+    }
+
+    private void engage() {
         entity.setMaxHealth(entity.getMaxHealth() * 5);
         entity.setHealth(entity.getMaxHealth());
         //noinspection ConstantConditions
@@ -42,9 +56,13 @@ public class LeanMob<T extends Monster> {
             name.append(s.substring(0, 1).toUpperCase()).append(s.substring(1).toLowerCase()).append(" ");
         }
 
-        entity.setCustomName(name.substring(0, name.length() - 1));
+        String newName = name.toString().trim();
+        entity.setCustomName(newName);
+        entity.setCustomNameVisible(true);
 
-        thread = Bukkit.getScheduler().scheduleSyncRepeatingTask(Supercave.INSTANCE, this::tick, 0, 1);
+        AutoBossbar.createBossBar(entity, 20, entity.getCustomName(), BarColor.PURPLE, BarStyle.SEGMENTED_6);
+
+        Bukkit.broadcast(new ComponentBuilder(ChatColor.YELLOW + "A " + newName + ChatColor.YELLOW + " has been engaged!").create());
     }
 
     private void tick() {
@@ -56,8 +74,7 @@ public class LeanMob<T extends Monster> {
 
         if (!engaged && entity.getHealth() < entity.getMaxHealth()) {
             engaged = true;
-            AutoBossbar.createBossBar(entity, 20, entity.getCustomName(), BarColor.PURPLE, BarStyle.SEGMENTED_6);
-            entity.setCustomNameVisible(true);
+            engage();
         }
 
         if (!engaged) return;
